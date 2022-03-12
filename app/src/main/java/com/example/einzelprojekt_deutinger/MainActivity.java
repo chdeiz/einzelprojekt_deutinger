7f04add143269a2320bff24f5036708941163556
Christian <christiandeutinger@hotmail.com>
2022-03-12 16:28:14 +0100

    matriclenumber input checks

--- app/src/main/java/com/example/einzelprojekt_deutinger/MainActivity.java
+++ app/src/main/java/com/example/einzelprojekt_deutinger/MainActivity.java
@@ -15,22 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Matrikelnummer input
-        TextView matnum = findViewById(R.id.inputMatnum);
-        String matriculationString = matnum.getText().toString();
-        int matriculationNumber = Integer.getInteger(matriculationString);
+        final Double maxMatricleNumber = 9.E9;
 
         Button btnSend = findViewById(R.id.inputButtonSend);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(matriculationString.isEmpty()){
+                // Matrikelnummer input
+                TextView matnum = findViewById(R.id.inputMatnum);
+                String matriculationString = matnum.getText().toString();
+                Double matriculationDouble;
+                Long matriculationNumber;
+                if (matriculationString == null || matriculationString.isEmpty()) {
                     Toast.makeText(MainActivity.this, "Matrikelnummer ist leer!\nBitte Matrikelnummer eingeben!", Toast.LENGTH_LONG).show();
+                } else if (matriculationString != null) {
+                    matriculationDouble = Double.parseDouble(matriculationString);
+                    if (matriculationDouble <= 0 || matriculationDouble >= maxMatricleNumber) {
+                        Toast.makeText(MainActivity.this, "Matrikelnummer ist ungültig!\nBitte eine gültige Matrikelnummer eingeben!", Toast.LENGTH_LONG).show();
+                    } else {
+                        matriculationNumber = Long.parseLong(matriculationString);
+                        TextView output = findViewById(R.id.outputChecksum);
+                        output.setText(Long.toString(matriculationNumber));
+                    }
                 }
-                TextView output = findViewById(R.id.outputChecksum);
-                output.setText(matnum.getText().toString());
-                matnum.toString().toCharArray();
             }
         });
     }
