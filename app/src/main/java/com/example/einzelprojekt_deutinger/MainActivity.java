6ed7c315cdd335cccf93f3f195105296dd490bf0
Christian <christiandeutinger@hotmail.com>
2022-03-12 15:49:40 +0100

    creating the input/output fields and button

--- app/src/main/java/com/example/einzelprojekt_deutinger/MainActivity.java
+++ app/src/main/java/com/example/einzelprojekt_deutinger/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.einzelprojekt_deutinger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Matrikelnummer input
+        TextView matnum = findViewById(R.id.inputMatnum);
+        String matriculationString = matnum.getText().toString();
+        int matriculationNumber = Integer.getInteger(matriculationString);
+
+        Button btnSend = findViewById(R.id.inputButtonSend);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(matriculationString.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Matrikelnummer ist leer!\nBitte Matrikelnummer eingeben!", Toast.LENGTH_LONG).show();
+                }
+                TextView output = findViewById(R.id.outputChecksum);
+                output.setText(matnum.getText().toString());
+                matnum.toString().toCharArray();
+            }
+        });
     }
 }
