6fbcd7c1df051edf97f8bed78640a4b9bd2b4cc4
Christian <christiandeutinger@hotmail.com>
2022-03-12 17:04:22 +0100

    checksum calculation

--- app/src/main/java/com/example/einzelprojekt_deutinger/MainActivity.java
+++ app/src/main/java/com/example/einzelprojekt_deutinger/MainActivity.java
@@ -15,14 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Double maxMatricleNumber = 9.E9;
+        final Double maxMatNumber = 9.E12; // max of 9 Billion people + 3 digits for uni + sem
+        final Double minMatNumber = 1000D; // min of three digits for university and semester
 
         Button btnSend = findViewById(R.id.inputButtonSend);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Matrikelnummer input
+                // matriculation number input
                 TextView matnum = findViewById(R.id.inputMatnum);
                 String matriculationString = matnum.getText().toString();
                 Double matriculationDouble;
@@ -31,15 +32,36 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Matrikelnummer ist leer!\nBitte Matrikelnummer eingeben!", Toast.LENGTH_LONG).show();
                 } else if (matriculationString != null) {
                     matriculationDouble = Double.parseDouble(matriculationString);
-                    if (matriculationDouble <= 0 || matriculationDouble >= maxMatricleNumber) {
+                    if (/*matriculationDouble < minMatNumber ||*/ matriculationDouble >= maxMatNumber) {
                         Toast.makeText(MainActivity.this, "Matrikelnummer ist ungültig!\nBitte eine gültige Matrikelnummer eingeben!", Toast.LENGTH_LONG).show();
                     } else {
+                        // mat. number is alright -> next steps send to Server and calc checksum
                         matriculationNumber = Long.parseLong(matriculationString);
                         TextView output = findViewById(R.id.outputChecksum);
-                        output.setText(Long.toString(matriculationNumber));
+                        String checksum = calcChecksum(matriculationNumber);
+                        output.setText("Quersumme Matrikelnummer:\n" + checksum);
                     }
                 }
             }
         });
+    }
+
+    private String calcChecksum(Long matriculationNumber) {
+        Long matnum = matriculationNumber;
+        int count = 0;
+        do {
+            matnum /= 10;
+            count++;
+        } while (matnum > 0);
+
+        matnum = matriculationNumber;
+        int checksum = 0;
+
+        for (int i = 0; i < count; i++) {
+            checksum += matnum % 10;
+            matnum/=10;
+        }
+
+        return Integer.toString(checksum);
     }
 }
